186cee4d84f76f759e69fea0077e3fd06e1c209c
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2025-10-20 12:54:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ if os.path.exists("local.env"):
 
 
 API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_HASH = getenv("API_HASH", "80d223dd670503fa6f017359f328cfe3") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6516561672").split()))
-OWNER_ID = int(getenv("OWNER_ID" ,"6516561672"))
-MONGO_URL = getenv("MONGO_URL", "None")
-BOT_TOKEN = getenv("BOT_TOKEN", "6689017907:AAGvzvyYdRC_71p4PTlYKGEv6Jg9TNe4Bps")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6753033991").split()))
+OWNER_ID = int(getenv("OWNER_ID" ,"6753033991"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://minhazan990:minhaz@cluster0.r2okalv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", "6798679157:AAEmY9jiypsA9GAzDIoevC3fKZGZ648N5R8")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://i.ibb.co/n3BJsk5/0820ba3e-d1a4-484c-a8c8-7e9eecb1ceee.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT", "hello")
 PM_LOGGER = getenv("PM_LOGGER", "the_rebel_princess_hindi_dubbed1")
 LOG_GROUP = getenv("LOG_GROUP", "the_rebel_princess_hindi_dubbed1")
 GIT_TOKEN = getenv("GIT_TOKEN", "None") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/Khanarman1234/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "BQCPOD2gQoFVcRJ0Q3QNS36-zzu1nLMwKBqXvu2-iAE5mMLypfcBccWCQr8FIhxKcRqydw5t0M0rkzm88k3xPWZeuRcb8X-kYJPvESRBQocg4rr_PouyCYO_MyYi4tC8BKlvcpnLyRnsi-Wc0t4cbwBBud39rZFPfEsXXDabRdt034QC8IMU9I8IhS3BM_0OXdsp-xUDJaRRuQfEcscURZMXG-z_Y0an7HzvCMiqrjBROmqJfJXu8RZc4mfyLVvogTrbvi1B3hgQTPxm48O_T3Jgq0NUblD368jOBgQQT3m6fPBu7GN7iVNAvZ0N777isCdYwYteZt-52FeedyZPyOMAAAAAAYRq1wgA")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQFwyGEACndwqps5TVDWFEUBs4KcrZTXkwcBQLgfyJJ3_SQX-hhV_WP54fS7A_vnQ_I6Ek-_JpU08llj54x0aUHTUhMcy5XltRBfjJ-TTgZfLRctLHRKyumeDeF4O9kdUP3_PDcImgmNcTbmdIWCIzNfM-m-thQYVsbPgQWOKCF2DPz9jAcMkf-XAuUCINmJvHjQE5SK7VgdhvSFPUpdQ-t6R8Y5y8_GC2xXVnwOc7r5EBkz86VYLw3WbORPzjOPclc-YoVRmyU5u7xngtuKFrRLm8UC223Ofa2KeBlYLqw5KeucCoeem-CzDnerHS1Ask-DC8F7t8919jq9w0ewEmOri0HqegAAAAGZBrlyAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "None")
 STRING_SESSION3 = getenv("STRING_SESSION3", "None")
 STRING_SESSION4 = getenv("STRING_SESSION4", "None")
